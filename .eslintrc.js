501b24fbb0bb961282364ef29d5c4b6c846892e5
linglong <linglongllzz@gmail.com>
2020-09-18 17:04:54 +0800

    fixed eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// module.exports = {
+//   standard: {},
+// }
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    // semi: 2,
+    // 'comma-dangle': ['off'],
+    // 'import/no-named-as-default': 0,
+  },
+}
